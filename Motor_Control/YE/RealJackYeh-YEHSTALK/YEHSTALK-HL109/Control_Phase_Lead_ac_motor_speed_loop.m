708a86884ab5c27b27cc1562e53ac4e856e53a8f
shun2023 <544361935@qq.com>
2025-09-22 10:31:53 +0800

    init

---
+++ Motor_Control/YE/RealJackYeh-YEHSTALK/YEHSTALK-HL109/Control_Phase_Lead_ac_motor_speed_loop.m
@@ -0,0 +1,33 @@
+s=tf('s'); Ts=1/4000;
+wsc=100; J=0.00016;  % 速度环帶寬wsc=100 rad/s
+Kp_w=J*wsc; Ki_w=Kp_w*wsc/5; % 設計PI控制器的Kp, Ki值
+K1 = 1; K2 = 0.1; K3 = 10; % 不同回路增益
+tf_pi=tf([Kp_w Ki_w],[1 0]); % PI控制器
+tf_plant=tf(1,[J 0]); % 受控廠
+tf_currLoop = tf(1000, [1 1000]); % 電流环設計為帶寬為1000 rad/s的低通濾波器
+tf_totalDelay = exp(-s*1.5*Ts); % 總延遲時間為1.5*Ts (PWM標準採樣法)
+[num1, den1] = pade(1.5*Ts, 1); % 使用一階Pade近似exp(-s*1.5*Ts)
+tf_totalDelay_Pade = tf(num1, den1); % 建立一階延遲Pade近似轉移函數
+Go_w_noDelay= tf_pi*tf_plant; % 計算開环傳遞函數
+Go_w_withDelay = tf_pi*tf_plant*tf_currLoop*tf_totalDelay_Pade;
+% Phase-lead controller (1+aTs)/(1+Ts), a>1
+phim = 70; % degree
+a=(1+sin(70*pi/180))/(1-sin(70*pi/180)); 
+wm = 3910; % rad/s, 對應未補償系統 -10*log10(a)的角頻率
+T = 1/(sqrt(a)*wm);
+G_phase_lead = tf([a*T 1], [T 1]);
+Go_w_withDelay_phase_lead = 28*Go_w_withDelay*G_phase_lead;
+h=bodeoptions;
+h.PhaseMatching='on';
+h.Title.FontSize = 14;
+h.XLabel.FontSize = 14;
+h.YLabel.FontSize = 14;
+h.TickLabel.FontSize = 14;
+figure(1)
+rlocus(Go_w_withDelay); legend('withDelay');
+figure(2)
+bodeplot(28*Go_w_withDelay,'-k', Go_w_withDelay_phase_lead, '-b',{1,10000},h);
+legend('No Compensated', 'Phase-lead Compensated');
+h = findobj(gcf,'type','line');
+set(h,'linewidth',2);
+grid on;
